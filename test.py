85988ff7fb3893de51d1e5bf5309c2577b77fb65
shashank bhatia <36273761+shashankbhatia15@users.noreply.github.com>
2024-01-18 10:11:29 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import os
 import streamlit as st
-gemini_key = os.environ.get('gemini_key')
+gemini_key = os.environ.get('GEMINI_KEY')
 
 # Now you can use the gemini_key in your Python project
 st.write(gemini_key)
