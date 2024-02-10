24cb2df2659e804b2dd4ccc4807851c083230953
shashank bhatia <36273761+shashankbhatia15@users.noreply.github.com>
2024-02-10 20:21:40 +0530

    Create render_test.py

---
+++ render_test.py
@@ -0,0 +1,92 @@
+import os
+
+import pathlib
+import textwrap
+
+import google.generativeai as genai
+import streamlit as st
+# Import your question-answering code here
+
+
+# Used to securely store your API key
+# from google.colab import userdata
+
+
+
+
+
+GOOGLE_API_KEY=os.getenv('GOOGLE_API_KEY') 
+# GOOGLE_API_KEY=st.secrets["GOOGLE_API_KEY"] #if you save secrets in streamlit directly.
+
+
+genai.configure(api_key=GOOGLE_API_KEY)
+
+model = genai.GenerativeModel('gemini-pro')
+# chat = model.start_chat(history=[])
+# chat
+# First
+import streamlit as st
+
+# st.title("💬 ShankGPT")
+st.markdown(
+    """
+    <style>
+    .fixed-text {
+        position: fixed;
+        top: 60px;
+        left: 20px;
+        background-color: grey;
+        padding: 10px;
+        font-size: 24px; /* Adjust the font size as needed */
+        font-weight: bold; /* Make the text bold */
+        border: 1px solid #ccc;
+        border-radius: 50px;
+        z-index: 1;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+
+
+# Display the fixed text
+st.markdown('<div class="fixed-text">💬 ShankGPT</div>', unsafe_allow_html=True)
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "user", "parts": '''you are an AI assistant
+    '''}]
+    
+    # [{"role": "user", "parts": '''you are an AI assistant who follows the following rules for all your answers - 
+    # 1. give outputs in proper readable formats. use space between each word you output. 
+    # 2. pformat all the codes in a proper readable format
+    # '''}]
+    
+    # response = model.generate_content(st.session_state.to_dict()['messages'])
+    # msg = response.text
+    # st.session_state.messages.append({"role": "model", "parts": msg})
+    # st.chat_message("assistant").write(msg)
+    
+    st.session_state.messages.append({"role": "model", "parts": "How can I help you?"})
+
+for msg in st.session_state.messages[1:]:
+    st.chat_message(msg["role"]).write(msg["parts"])
+
+if prompt := st.chat_input():
+
+
+    st.session_state.messages.append({"role": "user", "parts": prompt})
+    st.chat_message("user").write(prompt)
+    # st.write(st.session_state.to_dict()['messages'])
+
+    # response = model.generate_content(prompt)
+    response = model.generate_content(st.session_state.to_dict()['messages'])
+    
+    msg = response
+    st.session_state.messages.append({"role": "model", "parts": msg.text})
+    st.chat_message("assistant").write(msg.text)
+
+
+# z=st.session_state.to_dict()["messages"]
+# st.write(type(st.session_state.to_dict()['messages'][2]))
+# st.write(st.session_state.to_dict()['messages'])
